6769e744a86fed21c4d5e0c9546878952acd58bd
tmbu <54630225+tmbu@users.noreply.github.com>
2019-11-24 00:08:17 +0100

    C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tembu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/05 03:13:37 by tembu             #+#    #+#             */
+/*   Updated: 2019/11/14 16:05:49 by tembu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void		ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+void		ft_swap(char **a, char **b)
+{
+	char *tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int			ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int			main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < argc)
+	{
+		j = 1;
+		while (j <= argc - 2)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+				ft_swap(&argv[j], &argv[j + 1]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
