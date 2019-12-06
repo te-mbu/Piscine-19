7481d5bd018ed52b5048f3e789e53d89dd671d67
tmbu <54630225+tmbu@users.noreply.github.com>
2019-12-06 23:26:14 +0100

    Exam02

--- Rendu exam 02/ft_list_remove_if/ft_list_remove_if.c
+++ Rendu exam 02/ft_list_remove_if/ft_list_remove_if.c
@@ -5,14 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/15 17:53:20 by exam              #+#    #+#             */
-/*   Updated: 2019/11/15 18:29:05 by exam             ###   ########.fr       */
+/*   Created: 2019/11/22 11:13:30 by exam              #+#    #+#             */
+/*   Updated: 2019/11/22 11:29:21 by exam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "ft_list.h"
 
-void			ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void		ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
+	t_list *list;
+	t_list *next_list;
+	t_list *tmp;
 
+	if (begin_list && *begin_list)
+		list = *begin_list;
+	if (begin_list && *begin_list && cmp(list->data, data_ref) == 0)
+	{
+		next_list = list->next;
+		free(list);
+		*begin_list = next_list;
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	else if (begin_list && *begin_list)
+	{
+		next_list = list->next;
+		while (next_list)
+		{
+			if (cmp(next_list->data, data_ref) == 0)
+			{
+				tmp = next_list->next;
+				free(next_list);
+				next_list = tmp;
+				list->next = next_list;
+			}
+			else
+			{
+				list = next_list;
+				next_list = list->next;
+			}
+		}
+	}
 }
